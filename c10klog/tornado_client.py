c7a8ee328768417c8972b7166bb1b576d1ac155e
pdmurray <peynmurray@gmail.com>
2023-02-09 16:48:13 -0800

    Slow progress...

--- c10klog/tornado_client.py
+++ c10klog/tornado_client.py
@@ -1,46 +1,49 @@
+from typing import Optional
 from collections.abc import Awaitable
-from concurrent.futures import ThreadPoolExecutor
-# import asyncio
+import asyncio
 import pickle
 import logging
-from tornado.ioloop import IOLoop
-from tornado.iostream import IOStream, StreamClosedError
 from tornado.tcpclient import TCPClient
-
+import time
 
 
 class TCPClientHandler(logging.Handler):
-    """Lazyily connect via TCP to the driver when a log message is emitted."""
+    """Lazily connect via TCP to the driver when a log message is emitted."""
     def __init__(
         self,
-        host: str,
-        port: int = logging.handlers.DEFAULT_TCP_LOGGING_PORT
+        host: Optional[str] = 'localhost',
+        port: Optional[int] = logging.handlers.DEFAULT_TCP_LOGGING_PORT,
     ):
-        super().__init__(self)
+        super().__init__()
         self.client = TCPClient()
         self.host = host
         self.port = port
         self.stream = None
+        self.loop = None
 
     async def emit(self, record: logging.LogRecord) -> Awaitable[None]:
         """Emit a log record.
 
         Args:
-            record:
+            record: Log record to emit.
         """
+        print(f'calling emit {record.msg}')
         try:
             msg = self.format(record)
+            print('formatted')
             if not self.stream:
+                print('attempting connection')
                 self.stream = await self.client.connect(self.host, self.port)
+                print('connection made')
 
             if self.stream:
                 await self.stream.write(
-                    await self.serialize(msg)
+                    self.serialize(msg)
                 )
         except Exception as e:
             print(e)
 
-    async def serialize(log: logging.LogRecord) -> Awaitable[bytes]:
+    def serialize(self, log: logging.LogRecord) -> Awaitable[bytes]:
         """Serialize a log record in a separate thread, and await the result.
 
         Args:
@@ -49,12 +52,22 @@ class TCPClientHandler(logging.Handler):
         Returns:
             The serialized log record wrapped in an awaitable
         """
+        print('calling serialize')
+        return pickle.dumps(log)
 
-        return await IOLoop.current().run_in_executor(None, pickle.dumps, log)
-        # with ThreadPoolExecutor() as executor:
-            # return await asyncio.run_in_executor(executor, pickle.dumps, log)
+    def handle(self, record: logging.LogRecord) -> bool:
+        if record.name == '__main__':
+            if self.loop is None:
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+                # print('running forever')
+                # loop.run_forever()
 
-    async def handle(self, record: logging.LogRecord) -> bool:
+            loop.create_task(self._handle(record))
+            loop.run_until_complete(loop.shutdown_asyncgens())
+
+    async def _handle(self, record: logging.LogRecord) -> bool:
+        print('calling _handle')
         rv = self.filter(record)
         if rv:
             self.acquire()
@@ -65,21 +78,20 @@ class TCPClientHandler(logging.Handler):
         return rv
 
 
-# socket_handler = logging.handlers.SocketHandler(
-#     "localhost", logging.handlers.DEFAULT_TCP_LOGGING_PORT
-# )
-
-stream_handler = logging.StreamHandler()
+handler = TCPClientHandler()
 logging.basicConfig(
     level="DEBUG",
     format="%(asctime)s %(levelname)s %(name)s::%(message)s",
     datefmt="[%X]",
     handlers=[
-        socket_handler,
-        stream_handler,
+        handler,
+        logging.StreamHandler(),
     ],
 )
+logging.getLogger('asyncio').setLevel("DEBUG")
 
+logger = logging.getLogger(__name__)
+logger.info("Jackdaws love my big sphinx of quartz.")
 # while True:
-#     logging.info("Jackdaws love my big sphinx of quartz.", extra={"pid": os.getpid()})
-#     time.sleep(1)
+#     logger.info("Jackdaws love my big sphinx of quartz.")
+#     time.sleep(5)
